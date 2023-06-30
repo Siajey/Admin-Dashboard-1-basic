e123e82c962c5329f4bd990171f82222a68ba6cd
Sia Jey <siavashmjyjs@gmail.com>
2023-06-30 23:20:32 +0330

    profile form is complete now

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ import Dashboard from './scenes/dashboard'
 import Team from './scenes/team'
 import Contacts from './scenes/contacts'
 import Invoices from './scenes/invoices'
+import Form from './scenes/form'
 //import Bar from './scenes/bar'
-//import Form from './scenes/form'
 //import Pie from './scenes/pie'
 //import Line from './scenes/line'
 //import FAQ from './scenes/faq'
@@ -32,7 +32,7 @@ function App() {
               <Route path='/team' element={<Team />}></Route>
               <Route path='/contacts' element={<Contacts />}></Route>
               <Route path='/invoices' element={<Invoices />}></Route>
-              {/*<Route path="/form" element={<Form />}></Route>*/}
+              <Route path='/form' element={<Form />}></Route>
               {/*<Route path="/bar" element={<Bar />}></Route>*/}
               {/*<Route path="/pie" element={<Pie />}></Route>*/}
               {/*<Route path="/line" element={<Line />}></Route>*/}
