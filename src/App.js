37cbd5ea9185208f353714101801b19350ffd9ee
Sia Jey <siavashmjyjs@gmail.com>
2023-07-01 18:32:58 +0330

    calendar section created successfully

--- src/App.js
+++ src/App.js
@@ -9,12 +9,12 @@ import Team from './scenes/team'
 import Contacts from './scenes/contacts'
 import Invoices from './scenes/invoices'
 import Form from './scenes/form'
+import Calendar from './scenes/calendar/calendar'
 //import Bar from './scenes/bar'
 //import Pie from './scenes/pie'
 //import Line from './scenes/line'
 //import FAQ from './scenes/faq'
 //import Geography from './scenes/geography'
-//import Calendar from './scenes/calendar'
 
 function App() {
   const [theme, colorMode] = useMode()
@@ -28,17 +28,17 @@ function App() {
           <main className='content'>
             <TopBar />
             <Routes>
-              {/*<Route path="/" element={<Dashboard />}></Route>*/}
+              <Route path='/' element={<Dashboard />}></Route>
               <Route path='/team' element={<Team />}></Route>
               <Route path='/contacts' element={<Contacts />}></Route>
               <Route path='/invoices' element={<Invoices />}></Route>
               <Route path='/form' element={<Form />}></Route>
+              <Route path='/calendar' element={<Calendar />}></Route>
               {/*<Route path="/bar" element={<Bar />}></Route>*/}
               {/*<Route path="/pie" element={<Pie />}></Route>*/}
               {/*<Route path="/line" element={<Line />}></Route>*/}
               {/*<Route path="/faq" element={<FAQ />}></Route>*/}
               {/*<Route path="/geography" element={<Geography />}></Route>*/}
-              {/*<Route path="/calendar" element={<Calendar />}></Route>*/}
             </Routes>
           </main>
         </div>
--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,20 +1,17 @@
 import { Box } from '@mui/material'
 import Header from '../../components/Header'
 
-
 const DashBoard = () => {
   return (
-    <Box m='20x'>
-      <Box display='flex' justifyContent='space-between' alignItems='center'>
-        <Header
-          title='DASHBOARD'
-          subtitle='Welcome Back To Your Dashboard'
-        />
+    <Box m='20x' sx={{textAlign:'center',marginTop:'10px'}}>
+      <Box display='flex' justifyContent='center' alignItems='center' mb='50px'>
+        <Header title='DASHBOARD' subtitle=' Welcome Back To Your Dashboard' />
+      </Box>
+      <Box>
+        <img src={require('../../photo_2022-08-08_22-40-20.jpg')} alt='user-profile' style={{borderRadius:'20px'}}/>
       </Box>
     </Box>
   )
 }
-
-
 
 export default DashBoard
